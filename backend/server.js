35db63370c3078e569ea8a79f5dae39d02254938
NihalCode <160307501+NihalCode@users.noreply.github.com>
2025-01-02 12:33:25 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
-import { sections } from 'data/sections.js';
+import { sections } from './data/sections.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
